39944bccbc5088b785c5d74310b5aea53acc3b8a
hughest64 <hughest64@gmail.com>
2018-08-12 16:59:35 -0500

    added Recipe and Gravity models

--- gravity/models.py
+++ gravity/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """ some attributes of a beer recipe. """
+    created_at = models.DateTimeField(auto_now_add=True)
+    recipe_name = models.CharField(max_length=100)
+    original_gravity = models.DecimalField(max_digits=4, decimal_places=3)
+
+
+class SGravity(models.Model):
+    """ A set of measured specific gravities for a recipe. """
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    specific_gravity = models.DecimalField(max_digits=4, decimal_places=3)
+
+    class Meta:
+        verbose_name_plural = 'S Gravity'
